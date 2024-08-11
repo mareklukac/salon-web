fa7154187bc64b7ac5f220fa58abdbc743414058
Marek Lukac <marek.lukac08@gmail.com>
2024-08-11 03:39:19 +0200

    first commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import HeaderMenu from "./components/Layouts/Header/header-menu";
+
+
+function App() {
+  return <HeaderMenu />;
+}
+
+export default App;
