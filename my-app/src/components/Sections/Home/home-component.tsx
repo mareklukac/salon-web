83c19fccc1ce3f4a0a4711f7f8b6d1b1ffd43025
Marek Lukac <marek.lukac08@gmail.com>
2024-08-18 04:19:02 +0200

    changed color names

--- my-app/src/components/Sections/Home/home-component.tsx
+++ my-app/src/components/Sections/Home/home-component.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
       <img className="background-image" alt="" src={backroundImage}></img>
       <h1>Maya Beauty Salon</h1>
       <h3>Teším sa na Vašu návštevu</h3>
-      {/* <button className="order-button">OBJEDNAŤ</button> */}
+      <button className="order-button">OBJEDNAŤ</button>
     </div>
   );
 };
